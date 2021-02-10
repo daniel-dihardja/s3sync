d16e68b4f5ccb59b42454184cae5dcfe6d4b97c6
danieldihardja <daniel@dihardja.de>
2021-02-10 10:15:45 +0100

    added chokidar

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Import required AWS SDK clients and commands for Node.js
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const chokidar = require('chokidar');
 
+console.log(chokidar);
 
