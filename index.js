edad04ba4cb6567f1d7a19c3a1f500acdc1b0108
danieldihardja <daniel@dihardja.de>
2021-02-10 13:28:29 +0100

    added fast-glob

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// Import required AWS SDK clients and commands for Node.js
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const chokidar = require('chokidar');
-const fg = require('fast-glob');
 
-
+// /Users/danieldihardja/s3sync
