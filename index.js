557051248f1b9521d22846289a106ab2f76320a0
danieldihardja <daniel@dihardja.de>
2021-02-10 19:37:40 +0100

    file interactions

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const chokidar = require('chokidar');
 
+const config = {
+	directory: '/Users/danieldihardja/s3sync',
+	bucket: '',
+	region: ''
+}
+
+// Initialize watcher.
+const watcher = chokidar.watch(config.directory, { persistent: true });
+
+watcher
+	.on('add', path => add('added ' + path))
+	.on('change', path => change('changed' + path))
+	.on('unlink', path => remove('removed' + path))
+
+function add(path) {
+	console.log(path);
+}
+
+function change(path) {
+	console.log(path);
+}
+
+function remove(path) {
+	console.log(path)
+}
